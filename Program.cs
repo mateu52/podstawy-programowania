c1c4485d493db0b63471930b267ca26abe864e13
mateu52 <wuoelte@gmail.com>
2024-11-02 16:25:22 +0100

    podwojna liczba

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace MyConsoleApp
     {
         static void Main(string[] args)
         {
-            int[] oceny = new int[] {1, 3, 0, 2, 5};
+            int[] oceny = new int[] {1, 3,3, 0, 2, 5};
             int n = oceny.Length;
             int max = oceny[0];
             for(int i=0;i<n;i++){
